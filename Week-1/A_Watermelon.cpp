0105217f6f235c2c165b89deb6bdcd32a2b2a69d
Harshvardhan Jha <harshvardhan.j.jha@oracle.com>
2022-10-27 19:14:11 +0530

    Finished until Topic 3: Implementation Greedy is left

---
+++ Week-1/A_Watermelon.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+    int w;
+    cin >> w;
+    if(w%2==0 && w>2)
+        cout<<"YES"<<endl;
+    else
+        cout<<"NO"<<endl;
+}
